313b0b809ff4b314000710d4cad62789c289c7c4
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-27 15:58:06 -0300

    layout.php: se agregó script de sweetalert

--- views/layout.php
+++ views/layout.php
@@ -18,6 +18,7 @@
 
     <?php if (isset($script)) {
         echo "<script src='build/js/app.js'></script>";
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
     } ?>
 
     
