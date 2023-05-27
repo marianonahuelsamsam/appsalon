6367734f4556446a12793a95f5a59a12bbc7f69a
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-27 15:59:50 -0300

    apiController: se agregó el método "guardar"

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -2,11 +2,20 @@
 
 namespace Controller;
 
+use Model\Cita;
 use Model\Servicio;
 
 class APIController {
     public static function index(){
         $servicios = Servicio::all();
         echo json_encode($servicios);
+    }
+
+    public static function guardar() {
+        $cita = new Cita($_POST);
+
+        $resultado = $cita->guardar();
+
+        echo json_encode($resultado);
     }
 }
