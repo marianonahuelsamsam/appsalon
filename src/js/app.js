a9b20a777bd3bd41f28bdc29ae48a75aae08784e
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-27 15:56:19 -0300

    app.js: Reestructuración del código y subida al servidor

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,15 @@
+// Variables para ubicar la sección
 let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
 // Objeto de citas que se irá llenando con el nombre del usuario, la fecha y hora que elija y los servicios que seleccione.
 const cita = {
-   nombre: "",
-   fecha: "",
-   hora: "",
-   servicios: []
+    id: "",
+    nombre: "",
+    fecha: "",
+    hora: "",
+    servicios: []
 }
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -17,41 +19,40 @@ document.addEventListener("DOMContentLoaded", function() {
 function iniciarApp() {
     // Paginador
     tabs(); // Cambia la sección cuando se presionen los tabs (/citas).
-    mostrarSeccion(); // Es mandada a llamar por la función "tabs".
-    botonesPaginacion();
+    mostrarSeccion(); // Es mandada a llamar por la función "tabs" para mostrar la sección correspondiente.
+    botonesPaginacion(); // Define el comportamiento de los botones de paginación de la sección.
     paginaAnterior();
     paginaSiguiente();
 
-    consultarApi(); // Consultar a la base de datos conectando con el back-end (PHP).
-
+    consultarApi(); // Consultar a la base de datos conectando con el back-end (PHP) para obtener los servicios y mostrarlos en pantalla.
+    
+    // Llenado del objeto de Cita que enviaremos al servidor.
+    idCliente();
     nombreCLiente(); // Guardar el nombre del usuario en el objeto de citas.
     seleccionarFecha(); // Guardar la fecha que el usuario elija en el objeto de citas
     seleccionarHora(); // Guardar la hora que el usuario elija en el objeto de citas.
 
-    mostrarResumen(); // MOstrar el resumen de la cita.
+    mostrarResumen(); // Mostrar el resumen de la cita.
 }
 
-// Paginar las secciones.
+/* En un principio, ocultamos las 3 secciones de la página con código CSS. La clase "mostrar" contiene "display: block". 
+Para visibilizar cada sección nos basaremos en la variable "paso" definida en el top del código para mostrar solo una sección 
+por vez. */
 function mostrarSeccion() {
-    // Primero eliminamos la sección actual
-    const seccionAnterior = document.querySelector(".mostrar");
-
     /* Para no recibir errores en consola, nos aseguramos que la app no intente eliminar la sección si todavía no
     contiene la clase "mostrar"*/
+    const seccionAnterior = document.querySelector(".mostrar");
     if (seccionAnterior){
         seccionAnterior.classList.remove("mostrar");
     } 
 
     /* Seleccionamos la sección actual a través de su id ("paso-1"/"paso-2"/"paso-3"). Para especificar el paso
-    nos ayudamos de la variabe que definimos al principio del código ("paso").*/
+    nos ayudamos de la variabe que definimos al principio del código ("paso") que por default contiene 1.*/
     const pasoSelector = `#paso-${paso}`;
-
-    // Le agregamos la clase mostrar a la sección actual
     const seccion = document.querySelector(pasoSelector);
     seccion.classList.add("mostrar");
 
     // RESALTAR EL TAB ACTUAL
-    // Buscamos el tab que tenga la clase "actual" y si existe lo eliminamos.
     const tabAnterior = document.querySelector(".actual");
     if(tabAnterior) {
         tabAnterior.classList.remove("actual");
@@ -60,17 +61,17 @@ function mostrarSeccion() {
     tabActual.classList.add("actual");
 } 
 
+// Con esta función iteramos sobre los botones para reasignar la variable "paso" con el fin de ubicarnos y mostrar la sección correspondiente
+
 function tabs() {
-    // Seleccionamos todos los botones.
     const botones = document.querySelectorAll('.tabs button')
 
-    // Iteramos sobre los botones
     botones.forEach(boton => {
         boton.addEventListener("click", function(e) {
             paso = parseInt(e.target.dataset.paso); // Reasignamos la variable con el paso actual.
              
             mostrarSeccion(); // Mostrar las secciones según el tab.
-            botonesPaginacion(); //COnfiguración de los botones del paginador.
+            botonesPaginacion(); //Configuración de los botones del paginador.
         })
     })
 }
@@ -96,6 +97,7 @@ function botonesPaginacion () {
 
     mostrarSeccion();
 }
+
 function paginaAnterior() {
     paginaAnterior = document.querySelector("#anterior");
 
@@ -116,7 +118,7 @@ function paginaSiguiente() {
         botonesPaginacion();
     })
 }
-
+// Una vez definido el comportamiento de la paginación, consultamos al back-end para recibir los servicios desde la DB.
 async function consultarApi() {
 
     try {
@@ -127,13 +129,12 @@ async function consultarApi() {
     } catch (error) {
         
     }
-    
 }
 // Mandada a llamar por "consultarApi()". Recibimos el listado de servicios a través de la api.
 function mostrarServicios(servicios) {
-
+    
     servicios.forEach( servicio => {
-
+        // Destruturing al arreglo que recibimos de seleccionarServicio().
         const {id, nombre, precio} = servicio;
         // Creamos el párrafo para el nombre del servicio.
         const nombreServicio = document.createElement("P");
@@ -148,6 +149,7 @@ function mostrarServicios(servicios) {
         // Creamos el div para el precio y nombre del servicio
         const divServicio = document.createElement("DIV");
         divServicio.classList.add("servicio");
+
         divServicio.dataset.idServicio = id;
 
         divServicio.appendChild(nombreServicio);
@@ -164,7 +166,7 @@ function mostrarServicios(servicios) {
 }
 
 function seleccionarServicio(servicio) {
-    const {servicios} = cita; // Extraemos el arreglo del objeto de Citas.
+    const {servicios} = cita; // Extraemos el arreglo del objeto de Citas (top del código).
     const {id} = servicio; // Extraemos el id de el objeto servicio que nos retornó la función mostrarServicios().
 
     // Identificar el servicio al que se le da click.
@@ -186,6 +188,9 @@ function seleccionarServicio(servicio) {
     }
 }
 
+function idCliente() {
+    cita.id = document.querySelector("#usuarioId").value;
+}
 function nombreCLiente() {
     cita.nombre = document.querySelector("#nombre").value;
 }
@@ -237,20 +242,14 @@ function mostrarResumen() {
         return;
     } 
 
-    // Destructuring al objeto de cita con la informaión completa ya disponible.
+    // Destructuring al objeto de cita con la información completa ya disponible.
     const { nombre, fecha, hora, servicios} = cita;
 
-    // Insertar nombre del cliente, fecha y hora de la cita.
-    const nombreCita = document.createElement("P")
-    nombreCita.innerHTML = `<span>Nombre:</span> ${nombre}`;
-    const fechaCita = document.createElement("P");
-    fechaCita.innerHTML = `<span>Fecha:</span> ${fecha}`;
-    const horaCita = document.createElement("P");
-    horaCita.innerHTML = `<span>Hora:</span> ${hora}`;
-    resumen.appendChild(nombreCita);
-    resumen.appendChild(fechaCita);
-    resumen.appendChild(horaCita);
-
+    // Le damos un heading a la parte donde se muestra que servicio fueron seleccionados
+    const headingServicios = document.createElement("H3");
+    headingServicios.textContent = "Resumen de Servicios";
+    resumen.appendChild(headingServicios);
+    
     // Iteramos el arreglo con los servicios y lo insertamos en la página.
     servicios.forEach(servicio=> {
         const {id, nombre, precio} = servicio;
@@ -260,9 +259,9 @@ function mostrarResumen() {
 
         // Nombre y precio del servicio
         const textoServicio = document.createElement("P");
-        textoServicio.innerHTML = `<span>Nombre:</span>: ${nombre}`;
+        textoServicio.innerHTML = `<span>Nombre:</span> ${nombre}`;
         const precioServicio = document.createElement("P");
-        precioServicio.innerHTML = `<span>Precio</span>: ${precio}`;
+        precioServicio.innerHTML = `<span>Precio</span> ${precio}`;
 
         // Armamos el div con la información del servicio insertando nombre y precio en servicioContenedor
         servicioContenedor.appendChild(textoServicio);
@@ -271,6 +270,90 @@ function mostrarResumen() {
         resumen.appendChild(servicioContenedor);
     })
 
+    // Le damos un heading a la parte donde se muestran los datos del usuario que creó la cita.
+    const headingCita = document.createElement("H3");
+    headingCita.textContent = "Resumen de Cita";
+    resumen.appendChild(headingCita);
+
+    // Obtenemos la fecha formateada al español.
+    formatearFecha(fecha);
+
+    // Insertar nombre del cliente, fecha y hora de la cita.
+    const nombreCita = document.createElement("P")
+    nombreCita.innerHTML = `<span>Nombre:</span> ${nombre}`;
+    const fechaCita = document.createElement("P");
+    fechaCita.innerHTML = `<span>Fecha:</span> ${fechaFormateada}`;
+    const horaCita = document.createElement("P");
+    horaCita.innerHTML = `<span>Hora:</span> ${hora}`;
+
+    // Boton para reservar cita
+    const botonReserva = document.createElement("BUTTON");
+    botonReserva.classList.add("boton");
+    botonReserva.textContent = "Reservar Cita"
+    botonReserva.onclick = reservarCita;
+
+    resumen.appendChild(nombreCita);
+    resumen.appendChild(fechaCita);
+    resumen.appendChild(horaCita);
+    resumen.appendChild(botonReserva);
+}
+
+async function reservarCita() {
+
+    const { nombre, fecha, hora, servicios, id } = cita;
+    const idServicio = servicios.map(servicio => servicio.id);
+
+    datos = new FormData();
+    datos.append("usuarioId", id);
+    datos.append("nombre", nombre);
+    datos.append("fecha", fecha);
+    datos.append("hora", hora);
+    datos.append("servicios", idServicio);
+    
+    try {
+        // Peticion
+        const url = "http://localhost:3000/api/cita";
+
+        const respuesta = await fetch(url, {
+            method: "POST",
+            body: datos
+        });
+
+        const resultado = await respuesta.json();
+
+        if(resultado.resultado) {
+            Swal.fire({
+                icon: 'success',
+                title: 'Cita Creada',
+                text: 'Tu cita fue asignada correctamente',
+                button: "Ok"
+            }).then( () => {
+                window.location.reload();
+            })
+        }
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error al crear la cita',
+            text: 'Intente de nuevo más tarde',
+            button: "Ok"
+        })
+    }
+}
+
+function formatearFecha(fecha) {
+    // Formatear fecha a español
+    const fechaObj = new Date(fecha);
+    const mes = fechaObj.getMonth();
+    const dia = fechaObj.getDate() +2;
+    const year = fechaObj.getFullYear();
+
+    // Pasamos la fecha a UTC;
+    const fechaUTC = new Date(Date.UTC(year, mes, dia));
+
+    // Formateamos la fecha que pasamos a UTC
+    const opciones = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
+    fechaFormateada = fechaUTC.toLocaleDateString("Es-AR", opciones);    
 }
 
 function mostrarAlerta(mensaje, tipo, elemento, desaparece = true) {
