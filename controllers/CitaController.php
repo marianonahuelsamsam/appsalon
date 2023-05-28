abf5980e99d65196d38325924c6097c9a1b097f8
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-28 02:26:51 -0300

    CitaController.php: se protegió la ruta agregando la función correspondiente

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -7,7 +7,8 @@ use MVC\Router;
 class CitaController {
 
     public static function index (Router $router) {
-        
+        isAuth();
+
         $router->render("cita/index", [
             "nombre" => $_SESSION["nombre"],
             "id" => $_SESSION["id"]
