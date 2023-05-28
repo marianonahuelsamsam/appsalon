1d95e234d49d3594a29baad4324bc773e5b86aa2
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-28 02:25:45 -0300

    APIcontroller: controlador listo, se comunica con la BD correctamente

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -3,6 +3,7 @@
 namespace Controller;
 
 use Model\Cita;
+use Model\citaServicio;
 use Model\Servicio;
 
 class APIController {
@@ -12,10 +13,26 @@ class APIController {
     }
 
     public static function guardar() {
+        // Guarda la cita y recibe el id.
         $cita = new Cita($_POST);
-
         $resultado = $cita->guardar();
 
+        // Asignamos el id de la cita a la siguiente variable.
+        $idCitas = $resultado["id"];
+    
+        // Creamos un arreglo con los id de los servicios seleccionados
+        $idServicios = explode(",", $_POST["servicios"]);
+
+        foreach ($idServicios as $idServicio) {
+            $args = [   
+                "citasId" => $idCitas,
+                "serviciosId" => $idServicio
+            ];
+
+            $citasServicios = new citaServicio($args);
+            $citasServicios->guardar();
+        }
+ 
         echo json_encode($resultado);
     }
 }
