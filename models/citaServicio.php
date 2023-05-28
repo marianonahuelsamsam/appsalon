e91a043adf579e4f4c537ef36cd462e81a613ec4
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-05-28 02:23:28 -0300

    citaServicio: configuración del modelo añadida

--- models/citaServicio.php
+++ models/citaServicio.php
@@ -2,6 +2,18 @@
 
 namespace Model;
 
-class citaServicio {
-    
+class citaServicio extends ActiveRecord {
+    protected static $tabla = "citas_servicios";
+    protected static $columnasDB = ["id", "citasId", "serviciosId"];
+
+    public $id;
+    public $citasId;
+    public $serviciosId;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args["id"] ?? null;
+        $this->citasId = $args["citasId"] ?? "";
+        $this->serviciosId = $args["serviciosId"] ?? "";
+    }
 }
