a4156f426c8f893d446e5f7b618db5ce60ff687a
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-07-02 20:02:25 -0300

    Formulario para crear - actualizar servicios completado.

---
+++ views/servicios/formulario.php
@@ -0,0 +1,9 @@
+<div class="campo">
+    <label for="nombre">Nombre</label>
+    <input type="text" id="nombre" name="nombre" placeholder="Nombre del servicio" value="<?php echo $nombreServicio ?>">
+</div>
+
+<div class="campo">
+    <label for="precio">Precio</label>
+    <input type="number" id="precio" name="precio" placeholder="Precio del servicio" value="<?php echo $precio ?>" >
+</div>
