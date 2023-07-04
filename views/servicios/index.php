49b664ff39a20bd04cef1d87627575dae9471c70
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-07-04 11:03:44 -0300

    views/servicios/index.php: ahora se muestra alerta de cual fue el servicio actualizado.

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -5,13 +5,19 @@
 <h1 class="nombre-pagina">Servicios</h1> 
 <p class="descripcion-pagina">Administra los servicios</p>
 
+<?php $idServicioActualizado = $_GET["id"] ?? ""; ?>
+
 <ul class="lista-servicios">
     <?php foreach($servicios as $servicio) : ?>
-    
+        
         <li>
             <p>Nombre: <span><?php echo $servicio->nombre ?></span></p>
             <p>Precio: $<span><?php echo $servicio->precio ?></span></p>
 
+            <?php if($servicio->id === $idServicioActualizado) : ?>
+                <p class="alerta exito"> <span>Servicio actualizado</span> </p>
+            <?php endif ?>    
+            
             <div class="acciones">
                 <a href="/servicios/actualizar?id=<?php echo $servicio->id; ?>" class="boton">Actualizar</a>
 
