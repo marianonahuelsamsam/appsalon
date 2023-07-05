2c37615fbebd0a4deef97b624d9afc2f3bf68ba7
Mariano Sam Sam <marianonahuelsamsam@hotmail.com>
2023-07-05 01:19:12 -0300

    app.php: se incluyó a dotenv.

--- includes/app.php
+++ includes/app.php
@@ -3,6 +3,8 @@ use Dotenv\Dotenv;
 use Model\ActiveRecord;
 
 require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
 
 require 'funciones.php';
 require 'database.php';
